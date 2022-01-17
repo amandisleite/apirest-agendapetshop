03fd530524fa31c857f97b6f474b7c36214862b3
Amanda Leite <mandirleite@gmail.com>
2022-01-17 16:37:15 -0300

    métodos GET atualizados: retorna lista de atendimentos e busca por um id

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,7 +1,15 @@
 const Atendimento = require('../models/atendimento');
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('você está na rota de atendimentos - GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.listarAtendimento(res);
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        
+        Atendimento.buscaPorId(id, res);
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
