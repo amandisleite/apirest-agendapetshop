2675330ecbdcf8826b66c26917a05642885a7a44
Amanda Leite <mandirleite@gmail.com>
2022-01-17 12:53:22 -0300

    database mysql configurada

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const customExpress = require("./config/customExpress")
 
-const app = customExpress();
+const conexao = require("./database/connection")
 
-app.listen(3000, () => console.log("servidor rodando na porta 3000"))
+conexao.connect((err) => {
+    if(err) {
+        console.log('deu erro')
+    } else {
+        console.log('database conectada')
+        const app = customExpress();
+        
+        app.listen(3000, () => console.log("servidor rodando na porta 3000"))
+    }
+})
 
