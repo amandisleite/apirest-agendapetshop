65d8cfb3842f7b35f0f6588afcdf09b7a5f2bb45
Amanda Leite <mandirleite@gmail.com>
2022-01-17 15:30:12 -0300

    model atendimento criado e controller alterado para inserção de dados na database

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,8 +1,11 @@
+const Atendimento = require('../models/atendimento');
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('você está na rota de atendimentos - GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const atendimento = req.body;
+        Atendimento.adicionarAtendimento(atendimento);
         res.send('você está na rota de atendimentos - POST')
     
     })
