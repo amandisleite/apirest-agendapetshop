6d6c3364dba095bec99708bf19db5f6c2e8f88e7
Amanda Leite <mandirleite@gmail.com>
2022-01-17 16:17:53 -0300

    validação de erros sobre data e tamanho do nome do cliente feita

--- models/atendimento.js
+++ models/atendimento.js
@@ -7,16 +7,42 @@ class Atendimento {
     adicionarAtendimento(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-        const atendimentoComData = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?';
 
-        conexao.query(sql, atendimentoComData, (erro, resultados) => {
-            if(erro) {
-            res.status(400).json(erro)
-            } else {
-            res.status(201).json(resultados)
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 3;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'data deve ser maior ou igual a data atual'
+            }, {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'cliente deve ter pelo menos três caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido);
+
+        const existemErros = erros.length;
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoComData = {...atendimento, dataCriacao, data};
+    
+            const sql = 'INSERT INTO Atendimentos SET ?';
+    
+            conexao.query(sql, atendimentoComData, (erro, resultados) => {
+                if(erro) {
+                res.status(400).json(erro)
+                } else {
+                res.status(201).json(resultados)
+                }
+            })
+        }
+
     }  
 } 
 
