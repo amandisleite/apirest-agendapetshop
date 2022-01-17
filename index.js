eb62d8e264c1d01e81afaa0a0ec9765a58499153
Amanda Leite <mandirleite@gmail.com>
2022-01-17 13:04:05 -0300

    tabela criada no database

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const customExpress = require("./config/customExpress")
 
 const conexao = require("./database/connection")
+const tabelas = require("./database/tables")
 
 conexao.connect((err) => {
     if(err) {
         console.log('deu erro')
     } else {
         console.log('database conectada')
+
+        tabelas.init(conexao);
         const app = customExpress();
         
         app.listen(3000, () => console.log("servidor rodando na porta 3000"))
